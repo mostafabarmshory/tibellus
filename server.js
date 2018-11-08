b35294f5be658b7e614afdd7c8faec54820457e2
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-11-08 12:49:31 +0330

    Move to JS

--- server.js
+++ server.js
@@ -2,12 +2,17 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    chromeLocation: '/usr/bin/chromium'
+    chromeLocation: '/usr/bin/chromium',
+    pageLoadTimeout: 60* 1000,
+    waitAfterLastRequest: 60* 1000,
+    followRedirects: true
 });
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(prerender.removeStyle());
+server.use(prerender.removeAngularjs());
 
 server.start();
